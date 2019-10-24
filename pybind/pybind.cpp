bec5ec0f1c7955f1ef457b34906731b751024601
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2019-10-24 21:56:55 +0900

    継承下クラスをpybind11で扱う

--- pybind/pybind.cpp
+++ pybind/pybind.cpp
@@ -15,28 +15,57 @@ using namespace std;
 
 struct Test
 {
+    // abstract class
+    virtual ~Test() {}
+    virtual int test() const = 0;
+};
+
+struct Test1 : public Test
+{
     vector<int> a;
-    Test()
+    Test1()
     {
         a.resize(10);
-        for (int i = 0; i < 10; i++) a[i] = i; 
+        for (int i = 0; i < 10; i++) a[i] = i;
     }
     vector<int> get() const
     {
-        return a; 
+        return a;
     }
-    Test copy() const
+    Test1 copy() const
     {
-        Test test;
+        Test1 test;
         test.a = a;
         return test;
     }
     void up()
     {
-        for (int i = 0; i < 10; i++) a[i]++; 
+        for (int i = 0; i < 10; i++) a[i]++;
+    }
+    int test() const override
+    {
+        return 1;
     }
 };
 
+// 継承 https://pybind11.readthedocs.io/en/stable/advanced/classes.html
+
+struct PyTest : public Test
+{
+    using Test::Test;
+    int test() const override
+    {
+        PYBIND11_OVERLOAD_PURE(int, Test, test);
+    }
+};
+
+struct Test2 : public Test
+{
+    int test() const override
+    {
+        return 2;
+    }
+};
 
 #ifdef PY
 
@@ -44,11 +73,18 @@ namespace py = pybind11;
 PYBIND11_MODULE(pbtest, m) {
     m.doc() = "pybind test";
 
-    py::class_<Test>(m, "Test")
+    py::class_<Test, PyTest>(m, "Test")
     .def(pybind11::init<>(), "constructor")
-    .def("get", &Test::get, "get value")
-    .def("up", &Test::up, "add")
-    .def("copy", &Test::copy, "copy");
+    .def("test", &Test::test, "test");
+
+    py::class_<Test1>(m, "Test1")
+    .def("get", &Test1::get, "get value")
+    .def("up", &Test1::up, "add")
+    .def("copy", &Test1::copy, "copy")
+    .def("test", &Test1::test, "test");
+
+    py::class_<Test2>(m, "Test2")
+    .def("test", &Test2::test, "test");
 };
 
 #else
