2dad3d94ca527ff6545f1e3e5c068cc10e88bfbb
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2019-11-05 17:08:25 +0900

    実行ファイルのパスの取り方

---
+++ python/file_path.py
@@ -0,0 +1,7 @@
+import os
+
+print('path:        ', __file__)
+print('basename:    ', os.path.basename(__file__))
+print('dirname:     ', os.path.dirname(__file__))
+print('abs path:    ', os.path.abspath(__file__))
+print('abs dirname: ', os.path.dirname(os.path.abspath(__file__)))
