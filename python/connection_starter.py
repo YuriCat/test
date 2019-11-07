c47acd06dd1d68baac9537aacae0e3d08769a14f
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2019-11-07 15:41:17 +0900

    connection starting server

---
+++ python/connection_starter.py
@@ -0,0 +1,90 @@
+
+# リクエストに対して別プロセスを立てて接続する
+
+import io, time, struct, socket, pickle
+import multiprocessing as mp
+
+
+class PickledConnection:
+    def __init__(self, conn):
+        self.conn = conn
+
+    def _recv(self, size):
+        buf = io.BytesIO()
+        while size > 0:
+            chunk = self.conn.recv(size)
+            n = len(chunk)
+            print(size, n)
+            buf.write(chunk)
+            size -= n
+        return buf
+
+    def recv(self):
+        buf = self._recv(4)
+        size, _ = struct.unpack("!i", buf.getvalue())
+        return pickle.loads(self._recv(size))
+
+    def send(self, msg):
+        print('send ', msg)
+        self.conn.send(pickle.dumps(msg))
+
+class ConnectionStarter:
+    def __init__(self, run_target, args):
+        self.run_target = run_target
+        self.args = args
+
+    def loop(self):
+        server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_sock.bind(('', 9999))
+        server_sock.listen(100)
+
+        for i in range(100): # 限界数まで
+            conn, _ = server_sock.accept()
+            conn = PickledConnection(conn)
+            print('server connected!')
+            mp.Process(target=self.run_target, args=(self.args, conn, i), daemon=True).start()
+
+        server_sock.close()
+
+
+def server_target(args, conn, index):
+    # 受け入れ側処理
+    while True:
+        #msg = conn.recv()
+        #cnt = int(msg) + 1
+        #print('server %d counter %d' % (index, cnt))
+        time.sleep(1)
+        #conn.send(cnt)
+
+def client_target(args, conn, index):
+    # リクエスト側処理
+    print('client')
+    cnt = 0
+    while True:
+        print('client %d counter %d' % (index, cnt))
+        conn.send(cnt)
+        msg = conn.recv()
+        cnt = int(msg) + 1
+
+def connection_server():
+    # サーバ処理
+    starter = ConnectionStarter(server_target, [])
+    starter.loop()
+
+def open_socket_connection():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('', 9999))
+    return s
+
+def request_starter():
+    # リクエスト処理
+    for i in range(1):
+        conn = open_socket_connection()
+        conn = PickledConnection(conn)
+        print('client connected!')
+        mp.Process(target=client_target, args=([], conn, i)).start()
+
+if __name__ == '__main__':
+    mp.Process(target=connection_server).start()
+    time.sleep(1)
+    request_starter()
