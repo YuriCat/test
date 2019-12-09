c6ad66f57d5a2758ba4ff68896aafd615224cc37
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2019-12-09 15:38:58 +0900

    Python args, kwargsの使い方詳細

--- python/kwargs.py
+++ python/kwargs.py
@@ -2,5 +2,30 @@ def func(*args, **kwargs):
   print(args)
   print(kwargs)
 
+print('args, kwargs')
+
 func(a=0, b=1)
 func(1, a=0, b=1)
+
+args = {'a': 1, 'b': 3}
+
+print('args as dicts')
+
+func(*args)
+func(**args)
+
+print('function with orginal arguments')
+
+def func2(a, b):
+    print(a, b)
+
+func(*args)
+func2(**args)
+
+print('instance method')
+
+class A:
+    def __init__(self, a, b):
+        print(a, b)
+
+a = A(**args)
