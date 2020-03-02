e9f110a4bf43e81a8630c5a1e3d78b3eda65410f
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2020-03-02 15:27:43 +0900

    PyTorch埋め込みサンプル、保存やpickle

--- pytorch/embedding.py
+++ pytorch/embedding.py
@@ -4,11 +4,30 @@ import numpy as np
 import torch
 import torch.nn as nn
 
+
 emb = nn.Embedding(4, 2, padding_idx=0)
 
 idx = np.array([[[0, 1]]])
 tidx = torch.LongTensor(idx)
+print(emb(tidx))
 
-e = emb(tidx)
 
-print(e)
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.emb = nn.Embedding(4, 2, padding_idx=0)
+    def forward(self, x):
+        return self.emb(x)
+
+net = Net()
+print(net(tidx))
+torch.save(net.state_dict(), 'tmp.pth')
+
+net = Net()
+net.load_state_dict(torch.load('tmp.pth'))
+print(net(tidx))
+
+import pickle
+pnet = pickle.dumps(net)
+net = pickle.loads(pnet)
+print(net(tidx))
