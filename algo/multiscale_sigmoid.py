fd9daf885c81af7ca105c90e50b8c0b623ed45f6
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2020-07-18 08:58:24 +0900

    マルチスケールシグモイド可視化

---
+++ algo/multiscale_sigmoid.py
@@ -0,0 +1,18 @@
+
+import numpy as np
+
+def mssigm(x, a):
+    return 2 / (1 + np.exp(-x / a)) - 1
+
+
+x = np.arange(0, 10, 0.5).reshape((-1, 1))
+a = np.array([[1, 4]])
+
+v = mssigm(x, a)
+xs, ys = v[:,0], v[:,1]
+
+
+import matplotlib.pyplot as plt
+
+plt.scatter(xs, ys)
+plt.show()
