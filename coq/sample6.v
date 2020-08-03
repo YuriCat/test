f4ffc29b913c8af74f672e356d32c23c0185fe41
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2020-08-03 16:44:11 +0900

    sample6.vアップデート

--- coq/sample6.v
+++ coq/sample6.v
@@ -54,15 +54,104 @@ Qed.
 
 (*n以下の自然数にn+1が無いことの証明*)
 
-Fixpoint contains_eqnp1 (n : nat) :=
-  (fix contains_eq (n m : nat) :=
-     match n with
+Fixpoint contains_eqp1k (n k : nat) :=
+  match n with
+  | 0 => eqb n (n + 1 + k)
+  | S n' => orb (eqb n (n + 1 + k)) (contains_eqp1k n' k)
+  end.
+
+Lemma n_np1k_diff:
+  forall n k : nat, eqb n (n + 1 + k) = false.
+
+Proof.
+  induction n.
+  - simpl.
+    reflexivity.
+  - simpl.
+    apply IHn.
+Qed.
+
+Lemma contains_eqp1_false:
+  forall n k : nat, (contains_eqp1k n k) = false.
+
+Proof.
+  simpl.
+  induction n.
+  - simpl.
+    reflexivity.
+  - simpl.
+    intros k.
+    rewrite n_np1k_diff.
+    simpl.
+    apply IHn.
+Qed.
+
+Lemma n_p1m_diff:
+  forall n : nat, eqb (n + 1) n = false.
+
+Proof.
+  induction n.
+  - simpl.
+    reflexivity.
+  - simpl.
+    apply IHn.
+Qed.
+
+Lemma n_nm1k_diff:
+  forall n k : nat, eqb (n + 1) (n - k) = false.
+
+Proof.
+  induction k.
+  - simpl.
+    SearchPattern(_ - 0 _).
+
+  induction n.
+  - simpl.
+    reflexivity.
+  - induction k.
+    -- simpl.
+       apply n_p1m_diff.
+    -- simpl.
+       
+    
+Qed.
+
+
+Fixpoint contains_eq (n m : nat) :=
+  match n with
   | 0 => eqb n m
   | S n' => orb (eqb n m) (contains_eq n' m)
-  end) n (n + 1).
+  end.
+
+Fixpoint contains_eqnp1 (n : nat) :=
+  contains_eq n (n + 1).
 
 Compute contains_eqnp1 0.
 Compute contains_eqnp1 100.
+
+Lemma n_np1_diff:
+  forall n : nat, eqb n (n + 1) = false.
+
+Proof.
+  induction n.
+  - simpl.
+    reflexivity.
+  - simpl.
+    apply IHn.
+Qed.
+
+Lemma contains_eq_p1:
+  forall n : nat, contains_eq n (n + 1) = false.
+
+Proof.
+  induction n.
+  - simpl.
+    reflexivity.
+  - simpl.
+    rewrite n_np1_diff.
+    simpl.
+    rewrite IHn.
+
 
 Theorem contains_eq1_false:
   forall n : nat, contains_eqnp1 n = false.
@@ -71,9 +160,11 @@ Proof.
   induction n.
   - simpl.
     reflexivity.
-  - unfold contains_eqnp1.
+  - simpl.
+    rewrite n_np1_diff.
     simpl.
-    unfold contains_eq.
+    SearchPattern(_ || _).
+    rewrite n_Sn.
 
 
 
