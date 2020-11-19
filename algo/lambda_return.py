c2cf1ba5ea0e44dd3e9459e5373c2e9eb8b79684
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2020-11-20 01:02:47 +0900

    ラムダリターンにおけるbooststrap valueの扱いを更新

--- algo/lambda_return.py
+++ algo/lambda_return.py
@@ -4,7 +4,7 @@ a = [0.1, 0.2, 0.3, 0.4, 0.5]
 
 b = a + [0.5] * 100
 
-lmbda = 0.8
+lmbda = 0.7
 
 def lambda_return(l):
     if len(l) == 1:
@@ -13,6 +13,7 @@ def lambda_return(l):
         ll = lambda_return(l[1:])
         return [l[0] * (1 - lmbda) + ll[0] * lmbda] + ll
 
+print('terminal reward')
 print(lambda_return(a)[:10])
 print(lambda_return(b)[:10])
 
@@ -20,6 +21,7 @@ print(lambda_return(b)[:10])
 
 r = [0.1, 0, 0.2, 0.4]
 gamma = 0.9
+bootstrap_value = 0.2 / (1 - gamma)
 
 def compute_return(l):
     if len(l) == 1:
@@ -28,20 +30,23 @@ def compute_return(l):
         ll = compute_return(l[1:])
         return [l[0] + ll[0] * gamma] + ll
 
+print('immediate reward')
+print(r, bootstrap_value, '->')
 print(compute_return(r))
 print(sum(compute_return(r)))
 
-def lambda_return_with_gamma(l):
-    if len(l) == 1:
-        return [l[0]]
+def lambda_return_with_gamma(l, bv):
+    if len(l) == 0:
+        return [bv]
     else:
-        ll = lambda_return_with_gamma(l[1:])
+        ll = lambda_return_with_gamma(l[1:], bv)
         ret = l[0] + ll[0] * gamma
         return [ret * (1 - lmbda) + ll[0] * lmbda] + ll
 
-print(lambda_return_with_gamma(r))
+print(lambda_return_with_gamma(r, bootstrap_value))
+print(sum(lambda_return_with_gamma(r, bootstrap_value)))
 print(lambda_return(compute_return(r)))
 
-print(lambda_return_with_gamma(r + [0] * 100)[:5])
+print(lambda_return_with_gamma(r + [0] * 100, bootstrap_value)[:5])
 print(lambda_return(compute_return(r + [0] * 100))[:5])
 
