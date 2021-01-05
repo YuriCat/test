ade450751478b6b3092b9d60905ace25a8820886
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2021-01-05 23:57:49 +0900

    尤度統計調べる

---
+++ algo/likelihood_stats.py
@@ -0,0 +1,54 @@
+
+# 対数尤度のstatsはどういう値になるか？？
+
+import numpy as np
+
+
+A = 10
+N = 200000
+base = np.log(A)
+
+print(base)
+
+likelihood_sum = N * np.log(1 / A)
+
+print(base + likelihood_sum / N)
+
+likelihood_sum = 0
+for _ in range(N):
+    policy = np.random.dirichlet([1 / A] * A).astype(np.float64)
+    likelihood_sum += np.log(policy[0])
+
+print(base + likelihood_sum / N)
+
+likelihood_sum = 0
+for _ in range(N):
+    policy = np.random.dirichlet([1 / A] * A).astype(np.float64)
+    likelihood_sum += (policy * np.log(policy)).sum()
+
+print(base + likelihood_sum / N)
+
+likelihood_sum = 0
+for _ in range(N):
+    policy = np.random.dirichlet([1] * A).astype(np.float64)
+    likelihood_sum += (policy * np.log(policy)).sum()
+
+print(base + likelihood_sum / N)
+
+likelihood_sum = 0
+for _ in range(N):
+    policy = np.random.dirichlet([A] * A).astype(np.float64)
+    likelihood_sum += (policy * np.log(policy)).sum()
+
+print(base + likelihood_sum / N)
+
+likelihood_sum = 0
+for _ in range(N):
+    policy = np.random.dirichlet([10000] * A).astype(np.float64)
+    likelihood_sum += (policy * np.log(policy)).sum()
+
+print(base + likelihood_sum / N)
+
+likelihood_sum = N * A * (1 / A * np.log(1 / A))
+
+print(base + likelihood_sum / N)
