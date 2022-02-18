0d9a55dfd0e6fa330e02b5a6d1abe08436924b92
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2022-02-18 20:40:57 +0900

    fix: kl_loss近似を修正

--- algo/kl_loss.py
+++ algo/kl_loss.py
@@ -5,23 +5,37 @@ import torch.nn.functional as F
 
 x_ = np.random.randn(8).astype(np.float32)
 x = torch.tensor(x_, requires_grad=True)
-p = F.log_softmax(x)
-q = F.log_softmax(torch.randn(8))
+p = F.log_softmax(x, -1)
+p_ = p.detach()
+q = F.log_softmax(torch.randn(8), -1)
 
-kl_div = F.kl_div(p, torch.exp(q), reduction='sum')
+print(p)
+print(q)
+
+kl_div = F.kl_div(q, torch.exp(p), reduction='sum')
 print(kl_div)
 
 kl_div.backward()
 print(x.grad)
 
 x = torch.tensor(x_, requires_grad=True)
-p = F.log_softmax(x)
+p = F.log_softmax(x, -1)
+
+divided_kl_loss = 0
+for index in range(len(x)):
+    divided_kl_loss += (q[index] - p_[index]) * torch.exp(p_[index]) * -p[index]
+
+divided_kl_loss.backward()
+print(x.grad)
+
+x = torch.tensor(x_, requires_grad=True)
+p = F.log_softmax(x, -1)
 
 ex_kl_loss = 0
 for _ in range(100000):
     index = torch.exp(q).multinomial(num_samples=1, replacement=True)
-    p_ = p.detach()
-    ex_kl_loss += (q[index] - p_[index]) * torch.exp(p_[index]) / torch.exp(q[index]) * -p[index]
+    rho = torch.clamp(torch.exp(p_[index]) / torch.exp(q[index]), 0, 1e8)
+    ex_kl_loss += (q[index] - p_[index]) * rho * -p[index]
 
 ex_kl_loss.backward()
 print(x.grad / 100000)
