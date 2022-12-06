0073111f52cc1633aa447d8c82f1dfa7bba50bc9
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2022-12-06 18:22:04 +0900

    update off policy naive nash algorithm

--- algo/off_policy_naive_pg_rps.py
+++ algo/off_policy_naive_pg_rps.py
@@ -5,6 +5,9 @@ R = [
     [ 1, -1,  0],
 ]
 
+B = 100000
+T = 1000000
+
 import random
 import torch
 import torch.nn as nn
@@ -15,7 +18,7 @@ logit_regs = [torch.zeros(1, 3).detach()] * 31 + [logit.detach().clone()]
 pi_history = []
 
 replay_buffer = []
-for _ in range(10000):
+for _ in range(B):
     pi = F.softmax(torch.zeros(2, 3).detach(), -1)
     actions = torch.LongTensor([pi_.multinomial(num_samples=1, replacement=True) for pi_ in pi]).unsqueeze(-1)
     reward = R[actions[0]][actions[1]]
@@ -23,7 +26,7 @@ for _ in range(10000):
     replay_buffer.append((pi, actions, targets))
 
 
-for i in range(300000):
+for i in range(T):
     logit_ = logit
     if logit_.size(0) == 1:
         logit_ = logit_.repeat(2, 1)
@@ -33,19 +36,18 @@ for i in range(300000):
     reward = R[actions[0]][actions[1]]
     targets = torch.FloatTensor([reward, -reward]).unsqueeze(1)
 
-    replay_buffer.append((pi, actions, targets))
-    replay_buffer = replay_buffer[-10000:]
+    replay_buffer[random.randrange(len(replay_buffer))] = pi.gather(-1, actions), actions, targets
     mu, actions, targets = random.choice(replay_buffer)
 
     advantages = targets
-    selected_b_prob = mu.gather(-1, actions)
+    selected_b_prob = mu
     selected_t_prob = pi.gather(-1, actions)
-    rho = (selected_t_prob / selected_b_prob).prod(0, keepdim=True)
+    rho = selected_t_prob / selected_b_prob
+    c = torch.stack([rho[1], rho[0]])
 
-    loss = -F.log_softmax(logit_, -1).gather(-1, actions) * advantages * torch.clamp(rho, 0, 1)
-    #loss += F.kl_div(F.log_softmax(logit_, -1), mu, reduction='sum')
-    logit_reg = logit_regs[-random.choice([1, 2, 3, 5, 8, 13, 21])]
-    loss += F.kl_div(F.log_softmax(logit_, -1), F.softmax(logit_reg, -1), reduction='sum')
+    loss = -F.log_softmax(logit_, -1).gather(-1, actions).mul(advantages).mul(torch.clamp(rho, 0, 1)).mul(torch.clamp(c, 0, 1)).sum()
+    loss += 0.1 * F.kl_div(F.log_softmax(logit_, -1), F.softmax(logit_regs[-random.choice([1, 2, 3, 5, 8, 13, 21])], -1), reduction='sum')
+    loss += 0.1 * F.log_softmax(logit_, -1).gather(-1, actions).mul(torch.clamp(selected_t_prob / mu, 1e-4, 1e4) - 1).sum()
 
     loss.sum().backward()
     lr = 1e-3
