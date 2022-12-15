89d26256b0a804362f943d3f44d18aa343b7a302
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2022-12-15 17:51:01 +0900

    experiment: transfomer decoder layer

--- pytorch/transformer_layer.py
+++ pytorch/transformer_layer.py
@@ -2,11 +2,11 @@
 import torch
 import torch.nn as nn
 
-encoder = nn.TransformerEncoderLayer(d_model=128, nhead=4, dim_feedforward=256, batch_first=True)
-decoder = nn.TransformerDecoderLayer(d_model=128, nhead=4, dim_feedforward=256, batch_first=True)
+encoder = nn.TransformerEncoderLayer(d_model=128, nhead=4, dim_feedforward=256, dropout=0, batch_first=True)
+decoder = nn.TransformerDecoderLayer(d_model=128, nhead=4, dim_feedforward=256, dropout=0, batch_first=True)
 
-x = torch.zeros((1, 10, 128))
-y = torch.zeros((1, 20, 128))
+x = torch.randn((1, 10, 128))
+y = torch.randn((1, 20, 128))
 
 h = encoder(x)
 print(h.shape)
@@ -16,4 +16,10 @@ z = decoder(y, h)
 print(z.shape)
 
 print(z[0, :3, :8])
+
+h_ = torch.cat([h[:, -5:], h[:, :5]], 1)
+
+print(h_.shape)
+print(decoder(y, h_).shape)
+print(decoder(y, h_)[0, :3, :8])
 
