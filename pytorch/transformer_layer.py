aff3b7d11c63cb0416b6a751f54f7c77e469653c
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2023-07-08 01:21:55 +0900

    check transformer decoder layer

--- pytorch/transformer_layer.py
+++ pytorch/transformer_layer.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 
 encoder = nn.TransformerEncoderLayer(d_model=128, nhead=4, dim_feedforward=256, dropout=0, batch_first=True)
-decoder = nn.TransformerDecoderLayer(d_model=128, nhead=4, dim_feedforward=256, dropout=0, batch_first=True)
+decoder = nn.TransformerDecoderLayer(d_model=128, nhead=4, dim_feedforward=256, dropout=0, batch_first=True, norm_first=True)
 
 x = torch.randn((1, 10, 128))
 y = torch.randn((1, 20, 128))
@@ -15,11 +15,16 @@ print(h[0, :2, :8])
 z = decoder(y, h)
 print(z.shape)
 
-print(z[0, :3, :8])
+print(z[0, :6, :8])
 
 h_ = torch.cat([h[:, -5:], h[:, :5]], 1)
 
 print(h_.shape)
 print(decoder(y, h_).shape)
-print(decoder(y, h_)[0, :3, :8])
+print(decoder(y, h_)[0, :6, :8])
 
+y_ = torch.cat([y[:, -5:], y[:, :5]], 1)
+#y_ = y[:, :3]
+
+print(decoder(y_, h).shape)
+print(decoder(y_, h)[0, :6, :8])
